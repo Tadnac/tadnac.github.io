9c0508c9e9eb740f0e91b8358997de0ae9e92059
Radek Cvejn <106878089+Tadnac@users.noreply.github.com>
2025-03-12 07:30:46 +0100

    Create darkMode.js
    crete js scrip for enable switching between darkmode

---
+++ resources/JS/darkMode.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',function() {
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const body = document.body;
+  //chenge mode fn
+  function toggleDarkMode(){
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')
+        {
+      localStorage.setItem('darkMode','enabled');
+      }else{
+       localStorage.setItem('darkMode', 'disabled');
+      }
+  }
+  // load data from localStrage
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        body.classList.add('dark-mode');
+    }
+  // add listener for switch
+    darkModeToggle.addEventListener('click', toggleDarkMode);
+});
